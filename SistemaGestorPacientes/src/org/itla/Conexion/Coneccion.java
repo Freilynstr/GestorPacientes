7597a767873f472385bb62671a3a27c846e61101
freilynstr <freilynstr@hotmail.es>
2017-12-03 11:49:11 -0400

    Cambio de Coneccion a Conexion

--- SistemaGestorPacientes/src/org/itla/Conexion/Coneccion.java
+++ SistemaGestorPacientes/src/org/itla/Conexion/Coneccion.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.itla.Coneccion;
+package org.itla.Conexion;
 
 /**
  *
  * @author Santiago Pache
  */
-public class ConeccionMySQL {
+public interface Coneccion {
     
 }
